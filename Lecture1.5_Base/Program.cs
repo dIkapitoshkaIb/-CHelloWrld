b86b99ad0354bb7bda55aea02bcf6c9d9cd027a4
kapitoshka <burlingis.k@gmail.com>
2023-09-15 11:02:15 +0300

    +

--- Lecture1.5_Base/Program.cs
+++ Lecture1.5_Base/Program.cs
@@ -1,4 +1,5 @@
-﻿// 
+﻿// Нахождение максимально значения
+
 int a = 1;
 int b = 2;
 int c = 6;
@@ -6,7 +7,6 @@ int d = 8;
 int e = 4;
 
 int max = a;
-
 if (a > max) max = a;
 if (b > max) max = b;
 if (c > max) max = c;
