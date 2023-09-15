5408c0101898de53076d8d9e7eef911e615b6a7b
kapitoshka <burlingis.k@gmail.com>
2023-09-15 10:17:57 +0300

    добавил примеры сложения рандомных чисел в Lecture1.2_Sum

--- Lecture1.2_Sum/Program.cs
+++ Lecture1.2_Sum/Program.cs
@@ -1,6 +1,22 @@
-﻿//Сложение двух рандомных чисел с выводом их суммы.
+﻿//Сложение двух чисел.
 
 int numberA = 3;
 int numberB = 4;
+int result = numberA + numberB;
+Console.WriteLine(result);
+
+//Сложение двух рандомных чисел.
+
+int numberA = new Random().Next(1, 10); // 1 2 3 4 ... 9
+int numberB = new Random().Next(1, 10);
+int result = numberA + numberB;
+Console.WriteLine(result);
+
+//Сложение двух рандомных чисел c выводом чисел.
+
+int numberA = new Random().Next(1, 10); // 1 2 3 4 ... 9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
