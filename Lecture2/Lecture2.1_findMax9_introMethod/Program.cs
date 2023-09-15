188b7714a3e4f0554220c315b57d08008c26188c
kapitoshka <burlingis.k@gmail.com>
2023-09-15 23:17:05 +0300

    добавил методы поиска максимального значения

---
+++ Lecture2/Lecture2.1_findMax9_introMethod/Program.cs
@@ -0,0 +1,53 @@
+﻿// "стихийный" метод нахождения максимально значения циклом
+
+int a1 = 15, b1 = 21, c1 = 39,
+    a2 = 12, b2 = 23, c2 = 33,
+    a3 = 14, b3 = 23, c3 = 33,
+    max = a1;
+if (a1 > max) max = a1;
+if (b1 > max) max = b1;
+if (c1 > max) max = c1;
+
+if (a2 > max) max = a2;
+if (b2 > max) max = b2;
+if (c2 > max) max = c2;
+
+if (a3 > max) max = a3;
+if (b3 > max) max = b3;
+if (c3 > max) max = c3;
+
+Console.WriteLine(max);
+
+// пример нахождения через функцию
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+int a1 = 15, b1 = 21, c1 = 39,
+    a2 = 12, b2 = 23, c2 = 33,
+    a3 = 14, b3 = 23, c3 = 33;
+int max1 = Max(a1, b1, c1);
+int max2 = Max(a2, b2, c2);
+int max3 = Max(a3, b3, c3);
+int max = Max(max1, max2, max3);
+Console.WriteLine(max);
+
+// пример нахождения через функцию2.0
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+int a1 = 15, b1 = 21, c1 = 39,
+    a2 = 12, b2 = 23, c2 = 33,
+    a3 = 14, b3 = 23, c3 = 33;
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+Console.Write("max = ");
+Console.WriteLine(max);
