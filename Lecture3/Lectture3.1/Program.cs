fce4addde718c2273700c0f114b05e8720315ac7
dIkapitoshkaIb <burlingis.k@gmail.com>
2023-09-24 02:52:26 +0300

    добавил пункт по циклу "FOR"

---
+++ Lecture3/Lectture3.1/Program.cs
@@ -0,0 +1,30 @@
+﻿//Цикл "FOR"
+
+
+/* string Method4(int count, string text)
+{
+    string result = String.Empty;
+    for(int i = 0; i<count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+string res = Method4(5, "qwerty ");
+Console.WriteLine(res); */
+
+
+
+
+// Цикл "FOR" в цикле
+
+// Задача на составление таблицы умножения.
+
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} * {j} = {i * j}");          // Пример интерполяции сроки
+    }
+    Console.WriteLine();                                    // Требуется для отступа на строку после выполнения цикла в цикле
+}
