5368f4e90a290223d509c4eee72b8139022c39a2
dIkapitoshkaIb <burlingis.k@gmail.com>
2023-09-24 02:11:51 +0300

    Добавил виды функций и их примеры

---
+++ Lecture3/Program.cs
@@ -0,0 +1,84 @@
+﻿//Методы, функции
+
+
+
+
+
+// Вид 1 - ничего не принимают, ничего не возвращают.
+
+
+/* void Method1()
+{
+    Console.WriteLine("автор...")
+}
+Method1(); */
+
+
+
+
+
+
+// Вид 2 - что-то принимают, ничего не возвращают.
+
+
+/* void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+Method2("сообщение");
+
+// Пример:
+void Method22(string msg, int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+Method22("сообщение", 5); */
+
+
+
+
+
+// Вид 3 - ничего не принимают, что-то возвращают.
+
+
+/* int Method3()
+{
+    return;
+} */
+
+
+// Пример:
+/* int Method3()
+{
+    return DateTime.Now.Year;
+}
+
+int year = Method3();
+Console.WriteLine(year); */
+
+
+
+
+
+
+// Вид 4 - что-то принимают, что-то возвращают.
+
+
+/* string Method4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty;
+    while(i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+string res = Method4(5, "qwerty ");
+Console.WriteLine(res); */
