080c94d9e5bc045f1f1bcf0cf475265e3c4df69e
dIkapitoshkaIb <burlingis.k@gmail.com>
2023-09-25 16:32:45 +0300

    Добавил пример заполнения рандомными числами в двумерном массиве

--- Lecture4/Lecture4.1_DoubleMassive/Program.cs
+++ Lecture4/Lecture4.1_DoubleMassive/Program.cs
@@ -15,13 +15,13 @@ string[,] table = new string[2, 5];                 //задали "матриц
 //table[0,2]    table[1,4]  table[1,3]  ....   table[1,1]
 table[1,2] = "слово";
 
-for(int rows=0; rows<2;rows++)                      // Написали цикл для строк
+for(int rows=0; rows<2;rows++)                              // Написали цикл для строк
 {
-    for(int columns = 0; columns<5;columns++)       // "Вложили"(написали цикл в цикле)
-                                                    // код для столбцов
+    for(int columns = 0; columns<5;columns++)               // "Вложили"(написали цикл в цикле)
+                                                            // код для столбцов
     {
-        Console.WriteLine($"-{table[rows, columns]}-"); //для проверки, что количество строк и столбцов верно
-                                                      //нужно между фигурными скобками поставить знак "-"
+        Console.WriteLine($"-{table[rows, columns]}-");     //для проверки, что количество строк и столбцов верно
+                                                            //нужно между фигурными скобками поставить знак "-"
     }
 }
  */
@@ -30,25 +30,62 @@ for(int rows=0; rows<2;rows++)                      // Написали цикл
 
 int[,] matrix = new int[3,4];
 
-for(int i=0; i<3;i++)                      // Написали цикл для строк
+for(int i=0; i<3;i++)                                       // Написали цикл для строк
 {
-    for(int j = 0; j<4;j++)       // "Вложили"(написали цикл в цикле)
-                                                    // код для столбцов
+    for(int j = 0; j<4;j++)                                 // "Вложили"(написали цикл в цикле)
+                                                            // код для столбцов
     {
-        Console.WriteLine($"-{matrix[i, j]}-"); //для проверки, что количество строк и столбцов верно
-                                                      //нужно между фигурными скобками поставить знак "-"
+        Console.WriteLine($"-{matrix[i, j]}-");             //для проверки, что количество строк и столбцов верно
+                                                            //нужно между фигурными скобками поставить знак "-"
     }
 }
  */
-//Двумерные массивы с работой с числами2.0
+
+
+/* //Двумерные массивы с работой с числами2.0
 
 int[,] matrix = new int[3,4];
 
-for(int i=0; i<3;i++)
+for(int i=0; i<matrix.GetLength(0);i++)                     //Во время работы в массиве, можно использовать
+                                                            //функцию "название массива"GetLength
 {
-    for(int j = 0; j<4;j++)
+    for(int j = 0; j<matrix.GetLength(1);j++)
     {
         Console.Write($"{matrix[i, j]} ");
     }
     Console.WriteLine();
+} */
+
+
+
+//Двумерные массивы с работой с числами2.1
+//Заполнение матрица рандомными числами.
+
+int[,] matrix = new int[3, 4];
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
 }
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);        // [1, 10)
+        }
+    }
+}
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
